a011a424d995c78c4cb10b7286ebded91ebcbc37
JoseToitio <josetoitio@gmail.com>
2025-03-17 23:15:17 -0300

    feat: create back button

---
+++ frontend/components/BackButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goBack = () => {
+    router.back();
+}
+</script>
+
+<template>
+    <button @click="goBack" class="mb-4 bg-gray-200 p-2 rounded">
+        ← Voltar
+    </button>
+</template>
