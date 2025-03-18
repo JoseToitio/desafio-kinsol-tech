06d28d4bdf4c758f02b88438097c4873c59a7dc6
JoseToitio <josetoitio@gmail.com>
2025-03-17 23:13:39 -0300

    chore: nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  compatibilityDate: "2025-03-15",
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL || "http://127.0.0.1:8000/api", // URL da API Laravel
+    },
+  },
+});
